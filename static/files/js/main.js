5b777a9cb72c6e9241ce76366cff6708f073c4a9
adkdhinesh <150335746+adkdhinesh@users.noreply.github.com>
2023-11-09 18:45:45 +0530

    Add files via upload

---
+++ static/files/js/main.js
@@ -0,0 +1,24 @@
+const CHART=document.getElementById("chart")
+let lineChart=new hart(ctx,{
+	type:'line',
+	data:{
+		data:{
+		labels:["jeb",'feb','mar','apr','may','jun','Aug']
+	}
+});,{
+	type:"line",
+	data:{
+		labels:["jeb",'feb','mar','apr','may','jun']
+		datasets:[
+		{
+			label:'BTC'
+			data[29374,33537,49631,59828,36684,35572]
+			borderColor:red;
+		}
+		]
+	},
+	options:
+	{
+		responsive:true
+	}
+})
